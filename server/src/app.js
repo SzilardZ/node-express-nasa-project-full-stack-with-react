7cb588ba51eac4849901800942765afb39aeeb62
SzilardZ <szilard.zr@gmail.com>
2023-07-10 19:46:49 +0200

    version our node apis, v1

--- server/src/app.js
+++ server/src/app.js
@@ -3,9 +3,7 @@ import { fileURLToPath } from 'url';
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-
-import planetsRouter from './routes/planets/planets.router.js';
-import launchesRouter from './routes/launches/launches.router.js';
+import api from './routes/api.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -21,8 +19,7 @@ app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use('/planets', planetsRouter);
-app.use('/launches', launchesRouter);
+app.use('/v1', api);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
